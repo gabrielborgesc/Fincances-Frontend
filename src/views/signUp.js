73e0333771aaa89e11f6910464e09630ca461102
gabrielborgesc <jimmyrclgjf@gmail.com>
2020-12-22 14:07:12 -0300

    iniciando tela de cadastro

---
+++ src/views/signUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from '../components/card'
+
+class SingUp extends React.Component {
+    render(){
+        return(
+            <Card title = "Cadastro de Usuário">
+                Teste
+            </Card>
+        )
+    }
+}
+
+export default SingUp
