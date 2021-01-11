cef18a370fca20e8b78bd2b52b3bc5c69a61c696
gabrielborgesc <borges.gabrielc@gmail.com>
2021-01-11 20:34:30 -0300

    chegagem de senhas preechidas e iguais

--- src/views/recoverPassword/recoveringPassword.js
+++ src/views/recoverPassword/recoveringPassword.js
@@ -49,15 +49,49 @@ class RecoveringPassWord extends React.Component {
       }
     }
 
+    checkData = () => {
+        var check = true
+
+        if(!this.state.newPassword){
+            this.setState({errorNewPasswordMessage: "Campo senha é obrigatório"})
+            this.setState({inputNewPasswordErrorClass: "is-invalid"})    
+            check=false        
+        }
+        if(!this.state.confirmNewPassword){
+            this.setState({errorConfirmNewPasswordMessage: "Campo confirmação de senha é obrigatório"})
+            this.setState({inputConfirmNewPasswordErrorClass: "is-invalid"})    
+            check=false  
+        }
+        if(this.state.newPassword && this.state.confirmNewPassword && this.state.newPassword !== this.state.confirmNewPassword){
+            console.log('entrou')
+            this.setState({errorConfirmNewPasswordMessage: "As senhas não conferem"})
+            this.setState({inputConfirmNewPasswordErrorClass: "is-invalid"})
+            check=false
+        }
+        return check
+    }
+
+    resetView = () => {
+        this.setState( {errorNewPasswordMessage: null} )
+        this.setState( {inputNewPasswordErrorClass: null} )
+        this.setState( {errorConfirmNewPasswordMessage: null} )
+        this.setState( {inputConfirmNewPasswordErrorClass: null} )
+    }
+
     redefinePassword = () => {
-        this.userService.redefinePassword(this.props.hash, {
-            passwd: this.state.newPassword
-        }).then(response => {
-            popUp.successPopUp("Senha alterada com sucesso")
-            this.props.push('/login')
-        }).catch(error => {
-            HandleErrorService.handleError(this.props.push, error)
-        })
+        
+        this.resetView()
+
+        if(this.checkData())
+        {    this.userService.redefinePassword(this.props.hash, {
+                passwd: this.state.newPassword
+            }).then(response => {
+                popUp.successPopUp("Senha alterada com sucesso")
+                this.props.push('/login')
+            }).catch(error => {
+                HandleErrorService.handleError(this.props.push, error)
+            })
+        }
     }
 
     render(){
@@ -79,13 +113,14 @@ class RecoveringPassWord extends React.Component {
                                     </FormGroup>
                                     <FormGroup label = "Nova Senha: " htmlFor = "newPasswd">
                                         <input type="password"
-                                        className={"form-control " }
+                                        className={"form-control " + this.state.inputNewPasswordErrorClass }
                                         id="newPasswd"
                                         name="newPassword"
                                         value = {this.state.newPassword}
                                         onChange = { this.handleChange }
                                         onKeyPress={this.handleKeypress}
                                         placeholder="Password" />
+                                        <div className="invalid-feedback">{this.state.errorNewPasswordMessage}</div>
                                     </FormGroup>
                                     <FormGroup label = "Confirmação da nova Senha: " htmlFor = "confNewPasswd">
                                         <input type="password"
