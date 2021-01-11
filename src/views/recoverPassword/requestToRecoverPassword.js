c9523e08e73a940049851b20305677001951be47
gabrielborgesc <borges.gabrielc@gmail.com>
2021-01-11 20:30:23 -0300

    loading na página de envio de email até resposta da api. Falta tratamento de conferência de senha no recover

--- src/views/recoverPassword/requestToRecoverPassword.js
+++ src/views/recoverPassword/requestToRecoverPassword.js
@@ -6,6 +6,7 @@ import { BsCheck } from 'react-icons/bs'
 import EmailService from '../../app/service/emailService'
 import * as popUp from '../../components/toastr'
 import HandleErrorService from '../../app/service/handleErrorService'
+import ReactLoading from "react-loading";
 
 class RequestToRecoverPassWord extends React.Component {
 
@@ -15,7 +16,8 @@ class RequestToRecoverPassWord extends React.Component {
     }
 
     state = {
-        email: null
+        email: null,
+        loading: false
     }
 
     handleChange = (event) => {
@@ -32,51 +34,67 @@ class RequestToRecoverPassWord extends React.Component {
     }
 
     recoverPassword = () => {
+        this.setState({loading: true})
         this.emailService.recoverPassword({
             email: this.state.email,
             url: window.location.href
         }).then(response => {
+            this.setState({loading: false})
             popUp.successPopUp('Link para recuperação de senha enviado por email')
             this.props.push('/login')
         }).catch(error => {
+            this.setState({loading: false})
             HandleErrorService.handleError(this.props.push, error)
         })
-        popUp.infoPopUp("Enviando...")
         
     }
 
     render() {
-        return(
-            <div className = "row">
-                <div className = "col-md-6" style = { {position : 'relative', left : '300px'} }>
-                    <div className="bs-docs-section">
-                        <Card title = "Recuperar Senha">
-                            <fieldset>
-                                <FormGroup label = "Email: " htmlFor = "email">
-                                    <input type="email"
-                                    className={"form-control "}
-                                    name="email"
-                                    value = {this.state.email}
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Digite o Email"
-                                    onChange={this.handleChange}
-                                    onKeyPress={this.handleKeypress}/>
-                                </FormGroup>
-                                <button type="submit"
-                                        className="btn btn-success" 
-                                        onClick={this.recoverPassword}>
-                                        <BsCheck />  Enviar</button>
-                                <button className="btn btn-danger right-button"
-                                        onClick={() => this.props.push('/Login')} ><FaTimes />  Cancelar</button>
-                            
-                            </fieldset>
-                        </Card>
-                    </div>
+        if(this.state.loading){
+            return (
+                <div 
+                style = { { display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '600px'} }>
+                      <ReactLoading type={"bars"} color={"#000066"} />
                 </div>
+             )
+        }
+        else
+        {   
+            return(
+                <div className = "row">
+                    <div className = "col-md-6" style = { {position : 'relative', left : '300px'} }>
+                        <div className="bs-docs-section">
+                            <Card title = "Recuperar Senha">
+                                <fieldset>
+                                    <FormGroup label = "Email: " htmlFor = "email">
+                                        <input type="email"
+                                        className={"form-control "}
+                                        name="email"
+                                        value = {this.state.email}
+                                        id="exampleInputEmail1"
+                                        aria-describedby="emailHelp"
+                                        placeholder="Digite o Email"
+                                        onChange={this.handleChange}
+                                        onKeyPress={this.handleKeypress}/>
+                                    </FormGroup>
+                                    <button type="submit"
+                                            className="btn btn-success" 
+                                            onClick={this.recoverPassword}>
+                                            <BsCheck />  Enviar</button>
+                                    <button className="btn btn-danger right-button"
+                                            onClick={() => this.props.push('/Login')} ><FaTimes />  Cancelar</button>
+                                
+                                </fieldset>
+                            </Card>
+                        </div>
+                    </div>
 
-            </div>
-        )
+                </div>
+            )
+        }
     }
 }
 
